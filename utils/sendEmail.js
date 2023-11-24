87c99d4df770b1bed017ab2eb9bc5e4012c49e14
Arafath <arafath2092@gmail.com>
2023-11-24 16:34:20 +0530

    Done

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,10 +4,11 @@ import nodemailer from "nodemailer";
 const sendEmail = async (email, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.HOST,
-      service: process.env.SERVICE,
-      post: process.env.EMAIL_PORT,
-      secure: Boolean(process.env.SECURE),
+      // host: process.env.HOST,
+      // service: process.env.SERVICE,
+      // post: process.env.EMAIL_PORT,
+      // secure: Boolean(process.env.SECURE),
+      service: "gmail",
       auth: {
         user: process.env.USER,
         pass: process.env.PASS,
